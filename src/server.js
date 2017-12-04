a26be43b586a067c29c85473f7b8e48389b74220
David Furnes <david@dfurnes.com>
2017-12-04 14:00:11 -0500

    Shuffle things around.

--- src/server.js
+++ src/server.js
@@ -8,41 +8,41 @@ import webRoutes from './routes/web';
 
 const { APP_URL, APP_SECRET, PORT, REDIS_URL } = process.env;
 
+const app = express();
+
+// Configure view engine.
+app.set('views', path.resolve('src/views'));
+app.set('view engine', 'hbs');
+handlebars.registerPartials(path.resolve('src/views/partials'));
+
+// Serve static files.
+app.use(express.static('public'));
+
+// Configure sessions & authentication.
+const RedisStore = redis(session);
+app.use(
+  session({
+    secret: APP_SECRET,
+    store: new RedisStore({ url: REDIS_URL }),
+    cookie: {
+      maxAge: 1000 * 60 * 60, // 1 hour.
+      secure: app.get('env') === 'production',
+    },
+    saveUninitialized: false,
+    resave: false,
+  }),
+);
+
+// Trust proxies when running on Heroku.
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1);
+}
+
+// Register routes & start it up!
 (async () => {
-  const app = express();
-
-  // Configure view engine.
-  app.set('views', path.resolve('src/views'));
-  app.set('view engine', 'hbs');
-  handlebars.registerPartials(path.resolve('src/views/partials'));
-
-  // Serve static files.
-  app.use(express.static('public'));
-
-  // Configure sessions & authentication.
-  const RedisStore = redis(session);
-  app.use(
-    session({
-      secret: APP_SECRET,
-      store: new RedisStore({ url: REDIS_URL }),
-      cookie: {
-        maxAge: 1000 * 60 * 60, // 1 hour.
-        secure: app.get('env') === 'production',
-      },
-      saveUninitialized: false,
-      resave: false,
-    }),
-  );
-
-  // Trust proxies when running on Heroku.
-  if (app.get('env') === 'production') {
-    app.set('trust proxy', 1);
-  }
-
   app.use(apiRoutes);
   app.use(await webRoutes());
 
-  // Start it up!
   app.listen(PORT, () => {
     console.log(`GraphQL Server is now running on ${APP_URL}/graphql`);
     console.log(`View GraphiQL at ${APP_URL}/explore`);
