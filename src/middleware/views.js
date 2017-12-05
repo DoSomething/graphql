b572f8f5d47c4623bb69f1b55971504614173fcf
David Furnes <david@dfurnes.com>
2017-12-05 17:44:18 -0500

    Add environment badge.

--- src/middleware/views.js
+++ src/middleware/views.js
@@ -1,7 +1,10 @@
-const { NORTHSTAR_URL } = process.env;
+const { APP_ENV, NORTHSTAR_URL } = process.env;
 
 export default (req, res, next) => {
   res.locals.config = res.locals.config || {};
+
+  res.locals.config.production = APP_ENV === 'production';
+  res.locals.config.environment = APP_ENV;
   res.locals.config.northstarUrl = NORTHSTAR_URL;
 
   next();
