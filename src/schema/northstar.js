c98a49efcbdbb4897df2ff3fd059d62cfd67a7ea
David Furnes <david@dfurnes.com>
2017-12-06 16:53:41 -0500

    Add remaining fields to user schema.

--- src/schema/northstar.js
+++ src/schema/northstar.js
@@ -1,5 +1,6 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import gql from 'tagged-template-noop';
+import { GraphQLDate, GraphQLTime, GraphQLDateTime } from 'graphql-iso-date';
 import northstar from '../repositories/northstar';
 
 /**
@@ -8,11 +9,29 @@ import northstar from '../repositories/northstar';
  * @var {String}
  */
 const typeDefs = gql`
+  scalar Date
+
+  scalar DateTime
+
   # The user's role defines their abilities on any DoSomething.org site.
   enum Role {
     USER
     STAFF
     ADMIN
+  }
+
+  # The user's SMS subscription status.
+  enum SubscriptionStatus {
+    # User is actively subscribed to messaging.
+    ACTIVE
+    # User has requested to receive fewer broadcasts.
+    LESS
+    # User has texted STOP to unsubscribe from messaging.
+    STOP
+    # The mobile number is invalid, cannot receive texts, or texted STOP in the past.
+    UNDELIVERABLE
+    # An unknown issue exists with this user's SMS subscription.
+    UNKNOWN
   }
 
   # A DoSomething.org user profile.
@@ -23,10 +42,63 @@ const typeDefs = gql`
     firstName: String
     # The user's last name. Null if unauthorized.
     lastName: String
+    # The user's last initial.
+    lastInitial: String
+    # The user's avatar, uploaded via the mobile app.
+    photo: String
+      @deprecated(
+        reason: "The mobile app is discontinued, so users can no longer set this field."
+      )
+    # The user's email address.
+    email: String
+    # The user's mobile number.
+    mobile: String
+    # The user's birthdate, formatted YYYY-MM-DD.
+    birthdate: Date
+    # The user's street address. Null if unauthorized.
+    addrStreet1: String
+    # The user's extended street address (for example, apartment number). Null if unauthorized.
+    addrStreet2: String
+    # The user's city. Null if unauthorized.
+    addrCity: String
+    # The user's state. Null if unauthorized.
+    addrState: String
+    # The user's 6-digit zip code. Null if unauthorized.
+    addrZip: String
+    # The user's registration source. This is often a Northstar OAuth client.
+    source: String
+    # More information about the user's registration source (for example, a campaign or broadcast ID).
+    sourceDetail: String
+    # The user's Mobile Commons profile ID.
+    mobilecommonsId: String
+      @deprecated(
+        reason: "We no longer use Mobile Commons as our SMS provider. Use 'id' or 'mobile' instead."
+      )
+    # The user's SMS status.
+    smsStatus: SubscriptionStatus
+    # The user's conversation status will be paused if they are in a support conversation.
+    smsPaused: Boolean
+    # The user's language, as reported by their browser when they registered.
+    language: String
+    # The user's Drupal ID.
+    drupalId: String
+      @deprecated(
+        reason: "This will be removed in the future. Use 'id' instead."
+      )
     # The user's ISO-3166  country code.
     country: String
     # The user's role.
     role: Role
+    # The time this user was created. See the 'source' and 'source_detail' field for details.
+    createdAt: DateTime
+    # The last modified time for this user account.
+    updatedAt: DateTime
+    # The last time this user visited DoSomething.org on the web (accurate within an hour).
+    lastAccessedAt: DateTime
+    # The last time this user logged-in to DoSomething.org with their username & password.
+    lastAuthenticatedAt: DateTime
+    # The last time this user messaged DoSomething.org via Gambit.
+    lastMessagedAt: DateTime
   }
 
   type Query {
@@ -46,6 +118,8 @@ const resolvers = {
   Query: {
     user: (_, args, context) => northstar(context).users.load(args.id),
   },
+  Date: GraphQLDate,
+  DateTime: GraphQLDateTime,
 };
 
 /**
