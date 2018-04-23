d91753945e9a629627d9daf32495907ed57be5fe
David Furnes <david@dfurnes.com>
2018-04-23 16:20:04 -0400

    Update to 'esm'.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Enable ES module support in Node.
-require = require("@std/esm")(module);
+require = require('esm')(module);
 
 // Load environment variables from '.env'.
 require('dotenv').config();
@@ -8,4 +8,4 @@ require('dotenv').config();
 fetch = require('node-fetch');
 
 // Start the server.
-module.exports = require("./src/server.js").default;
+module.exports = require('./src/server.js').default;
