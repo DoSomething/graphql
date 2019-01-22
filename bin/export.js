d04edce4ad1c63262f104fd80b6a7b389a293b40
David Furnes <david@dfurnes.com>
2019-01-22 14:57:43 -0500

    Export schema to SDL & push to Apollo on deploys.

---
+++ bin/export.js
@@ -0,0 +1,15 @@
+// Register 'esm' and 'dotenv'.
+require = require('esm')(module);
+require('dotenv').config();
+
+const { writeFile } = require('fs');
+const { printSchema } = require('graphql');
+const schema = require('../src/schema').default;
+
+// Transform stitched schema into SDL & print to a git-ignored file.
+writeFile(`${__dirname}/../schema.graphql`, printSchema(schema), err => {
+  if (err) throw err;
+
+  console.log("Exported schema to 'schema.graphql'");
+  process.exit();
+});
