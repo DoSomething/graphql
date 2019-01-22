968fa74d29f782b82e1cd719438dfe3f800f3e37
Aaron Schachter <aaronschachter@gmail.com>
2019-01-22 09:49:49 -0800

    Updates syntax for field descriptions

--- src/schema/gambitContent.js
+++ src/schema/gambitContent.js
@@ -5,17 +5,17 @@ import { getConversationTriggers } from '../repositories/gambitContent';
 import Loader from '../loader';
 
 const entryFields = `
-  # The entry ID.
+  "The entry ID."
   id: String
-  # The entry name.
+  "The entry name."
   name: String
-  # The entry content type (e.g. 'photoPostConfig', 'askYesNo').
+  "The entry content type (e.g. 'photoPostConfig', 'askYesNo')."
   contentType: String
 `;
 
 const broadcastFields = `
   ${entryFields}
-  # Message to broadcast
+  "Message to broadcast"
   text: String
 `;
 
@@ -25,114 +25,115 @@ const broadcastFields = `
  * @var {String}
  */
 const typeDefs = gql`
-  # A DoSomething.org conversation topic.
+  "A DoSomething.org conversation topic."
   interface Topic {
     ${entryFields}
   }
 
-  # Topic for sending a single auto-reply message.
+  "Topic for sending a single auto-reply message."
   type AutoReplyTopic implements Topic {
     ${entryFields}
-    # The auto reply text.
+    "The auto reply text."
     autoReply: String!
   }
 
-  # Topic for creating a signup and sending a single auto-reply message.
+  "Topic for creating a signup and sending a single auto-reply message."
   type AutoReplySignupTopic implements Topic {
     ${entryFields}
+    "The campaign ID to create signup for if conversation changes to this topic."
     campaignId: Int!
-    # The auto reply text.
+    "Template sent as an auto reply to user messages."
     autoReply: String!
   }
 
-  # Topic for creating signup and photo posts. Asks user to text START to begin a photo post.
+  "Topic for creating signup and photo posts. Asks user to reply with START to create a draft photo post."
   type PhotoPostTopic implements Topic {
     ${entryFields}
-    # The campaign to create signup and photo post for if conversation changes to this topic.
+    "The campaign ID to create signup and photo post for if conversation changes to this topic."
     campaignId: Int!
-    # Template sent until user replies with START to begin a photo post.
+    "Template sent until user replies with START to begin a photo post."
     startPhotoPostAutoReply: String!
-    # Template that asks user to reply with quantity.
+    "Template that asks user to reply with quantity."
     askQuantity: String!
-    # Template that asks user to resend a message with valid quantity.
+    "Template that asks user to resend a message with valid quantity."
     invalidQuantity: String!
-    # Template that asks user to reply with a photo.
+    "Template that asks user to reply with a photo."
     askPhoto: String!
-    # Template that asks user to resend a message with a photo.
+    "Template that asks user to resend a message with a photo."
     invalidPhoto: String!
-    # Template that asks user to reply with a photo caption.
+    "Template that asks user to reply with a photo caption."
     askCaption: String!
-    # Template that asks user to resend a message with a valid photo caption.
+    "Template that asks user to resend a message with a valid photo caption."
     invalidCaption: String!
-    # Template that asks user to reply with why participated.
+    "Template that asks user to reply with why participated."
     askWhyParticipated: String!
-    # Template that asks user to resend a message with a valid why participated.
+    "Template that asks user to resend a message with a valid why participated."
     invalidWhyParticipated: String!
-    # Template that confirms a photo post was created.
+    "Template that confirms a photo post was created."
     completedPhotoPost: String!
-    # Template sent after photo post confirmation. User can text START to submit another photo post.
+    "Template sent after photo post confirmation. User can send START to submit another photo post."
     completedPhotoPostAutoReply: String!
   }
 
-  # Topic for creating signup and text posts. Ask user to text back a test post.
+  "Topic for creating signup and text posts. Ask user to reply with a text post."
   type TextPostTopic implements Topic {
     ${entryFields}
-    # The campaign to create signup and text post for if conversation changes to this topic.
+    "The campaign ID to create signup and text post for if conversation changes to this topic."
     campaignId: Int!
-    # Template that asks user to resend a message with valid text post.
+    "Template that asks user to resend a message with valid text post."
     invalidText: String!
-    # Template that confirms a text post was created. Replying to this creates another text post.
+    "Template that confirms a text post was created. Replying to this creates another text post."
     completedTextPost: String!
   }
 
-  # A DoSomething.org broadcast.
+  "A DoSomething.org broadcast."
   interface Broadcast {
     ${broadcastFields}
   }
 
-  # Broadcast that asks user a yes or no question, and changes topic to its own ID.
+  "Broadcast that asks user a yes or no question, and changes topic to its own ID."
   type AskYesNoBroadcastTopic implements Broadcast & Topic {
     ${broadcastFields}
-    # Message sent if user says yes.
+    "Message sent if user says yes."
     saidYes: String!
-    # The topic ID to change conversation to if user says yes
+    "The topic ID to change conversation to if user says yes"
     saidYesTopicId: String!
-    # The topic to change conversation to if user says yes.
+    "The topic to change conversation to if user says yes."
     saidYesTopic: Topic
-    # Message sent if user says yes
+    "Message sent if user says yes"
     saidNo: String!
-    # The topic ID to change conversation to if user says no.
+    "The topic ID to change conversation to if user says no."
     saidNoTopicId: String!
-    # The topic to change conversation to if user says no.
+    "The topic to change conversation to if user says no."
     saidNoTopic: Topic
-    # Message sent until user responds with yes or no.
+    "Message sent until user responds with yes or no."
     invalidAskYesNoResponse: String!
   }
 
-  # Broadcast that changes topic to an auto reply topic.
+  "Broadcast that changes topic to an AutoReplyTopic."
   type AutoReplyBroadcast implements Broadcast {
     ${broadcastFields}
-    # The auto reply topic ID to change conversation to.
+    "The ID of the AutoReplyTopic to change conversation to."
     topicId: String!
-    # The auto reply topic to change conversation to.
+    "The AutoReplyTopic to change conversation to."
     topic: AutoReplyTopic
   }
 
-  # Broadcast that asks user to reply with START and changes topic a photo post topic.
+  "Broadcast that asks user to reply with START and changes topic to a PhotoPostTopic."
   type PhotoPostBroadcast implements Broadcast {
     ${broadcastFields}
-    # The photo post topic ID to change conversation to.
+    "The ID of the PhotoPostTopic to change conversation to."
     topicId: String!
-    # The photo post topic to change conversation to.
+    "The PhotoPostTopic to change conversation to."
     topic: PhotoPostTopic
   }
 
-  # Broadcast that asks user to reply with a text post and changes topic to a text post topic.
+  "Broadcast that asks user to reply with a text post and changes topic to a TextPostTopic."
   type TextPostBroadcast implements Broadcast {
     ${broadcastFields}
-    # The text post Topic ID to change conversation to.
+    "The ID of the TextPostTopic to change conversation to."
     topicId: String!
-    # The text post topic to change conversation to.
+    "The TextPostBroadcast to change conversation to."
     topic: TextPostTopic
   }
 
@@ -140,26 +141,26 @@ const typeDefs = gql`
     ${broadcastFields}
   }
 
-  # A conversation trigger, used to change topic or answer a FAQ.
+  "A conversation trigger, used to change topic or answer a FAQ."
   type ConversationTrigger {
-    # The Contentful entry id.
+    "The entry id."
     id: String!
-    # The Rivescript trigger used to match an inbound message from a user.
+    "The Rivescript trigger used to match an inbound message from a user."
     trigger: String!
-    # The Rivescript reply to send user if their inbound message matches the trigger.
+    "The Rivescript reply to send user if their inbound message matches the trigger."
     reply: String!
-    # The topic ID to change user conversation to.
+    "The topic ID to change user conversation to."
     topicId: String
-    # The topic to change user conversation to.
+    "The topic to change user conversation to."
     topic: Topic
   }
 
   type Query {
-    # Get a broadcast by ID.
+    "Get a broadcast by ID."
     broadcast(id: String!): Broadcast
-    # Get all conversation triggers.
+    "Get all conversation triggers."
     conversationTriggers: [ConversationTrigger]
-    # Get a topic by ID.
+    "Get a topic by ID."
     topic(id: String!): Topic
   }
 `;
--- src/schema/gambitConversations.js
+++ src/schema/gambitConversations.js
@@ -19,81 +19,81 @@ import {
 const typeDefs = gql`
   scalar DateTime
 
-  # A DoSomething.org conversation.
+  "A DoSomething.org user conversation."
   type Conversation {
-    # The conversation ID.
+    "The conversation ID."
     id: String!
-    # The Northstar user ID of the user this conversation is with.
+    "The Northstar user ID of the user this conversation is with."
     userId: String!
-    # The conversation platform (e.g. 'sms', 'gambit-slack').
+    "The conversation platform (e.g. 'sms', 'gambit-slack')."
     platform: String!
-    # The time when this conversation was originally created.
+    "The time when this conversation was originally created."
     createdAt: DateTime
-    # The time this conversation was last modified.
+    "The time this conversation was last modified."
     updatedAt: DateTime
-    # The topic ID of current conversation topic.
+    "The topic ID of current conversation topic."
     topicId: String!
-    # The messages in this conversation.
+    "The messages in this conversation."
     messages: [Message]
   }
 
-  # A conversation message.
+  "A conversation message."
   type Message {
-    # The message ID.
+    "The message ID."
     id: String!
-    # The message direction.
+    "The message direction."
     direction: String!
-    # The user ID this message is from or to.
+    "The user ID this message is from or to."
     userId: String
-    # The time when this conversation was originally created.
+    "The time when this conversation was originally created."
     createdAt: DateTime
-    # The time this conversation was last modified.
+    "The time this conversation was last modified."
     updatedAt: DateTime
-    # The topic ID of the conversation topic when message was created.
+    "The topic ID of the conversation topic when message was created."
     topicId: String
-    # The message text
+    "The message text."
     text: String
-    # The message template (if outbound)
+    "The message template (if outbound)."
     template: String
-    # The Rivescript match (if inbound)
+    "The Rivescript trigger that was matched."
     match: String
   }
 
   type Query {
-    # Get a conversation by ID.
+    "Get a conversation by ID."
     conversation(id: String!): Conversation
-    # Get a paginated collection of conversations.
+    "Get a paginated collection of conversations."
     conversations(
-      # The page of results to return.
+      "The page of results to return."
       page: Int = 1
-      # The number of results per page.
+      "The number of results per page."
       count: Int = 20
     ): [Conversation]
-    # Get a paginated collection of conversations by user ID.
+    "Get a paginated collection of conversations by user ID."
     conversationsByUserId(
-      # The Northstar user ID to filter posts by.
+      "The user ID to filter conversations by."
       id: String!
-      # The page of results to return.
+      "The page of results to return."
       page: Int = 1
-      # The number of results per page.
+      "The number of results per page."
       count: Int = 20
     ): [Conversation]
-    # Get a message by ID.
+    "Get a message by ID."
     message(id: String!): Message
-    # Get a paginated collection of messages.
+    "Get a paginated collection of messages."
     messages(
-      # The page of results to return.
+      "The page of results to return."
       page: Int = 1
-      # The number of results per page.
+      "The number of results per page."
       count: Int = 20
     ): [Message]
-    # Get a paginated collection of messages by conversation ID.
+    "Get a paginated collection of messages by conversation ID."
     messagesByConversationId(
-      # The conversation ID to filter messages by.
+      "The conversation ID to filter messages by."
       id: String!
-      # The page of results to return.
+      "The page of results to return."
       page: Int = 1
-      # The number of results per page.
+      "The number of results per page."
       count: Int = 20
     ): [Message]
   }
