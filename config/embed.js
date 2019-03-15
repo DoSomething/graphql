766fd8611ba802c916852e2a6de0e962dd306927
David Furnes <david@dfurnes.com>
2019-03-15 10:38:14 -0400

    Remove old "prefer" config.

--- config/embed.js
+++ config/embed.js
@@ -11,32 +11,15 @@ export default {
 
   /**
    * We whitelist "safe" domains to embed rich content from, to prevent
-   * malicious content from being injected onto a page.
+   * malicious HTML content from being injected onto a page.
    *
    * @type {Array}
    */
   whitelist: [
-    'dosomething.org',
-    'buzzfeed.com',
-    'buzzfeednews.com',
     'facebook.com',
     'instagram.com',
-    'nytimes.com',
     'twitter.com',
     'youtu.be',
     'youtube.com',
-  ],
-
-  /**
-   * For some sites, we prefer OpenGraph/Twitter metatags. Note that these
-   * sites still must be whitelisted above!
-   *
-   * @type {Array}
-   */
-  preferMetatags: [
-    'buzzfeed.com',
-    'buzzfeednews.com',
-    'dosomething.org',
-    'nytimes.com',
   ],
 };
