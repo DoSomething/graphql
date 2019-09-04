1ff7ceeb0c5ae501872938cb620e56a91f1f03f6
David Furnes <david@dfurnes.com>
2019-09-04 12:51:46 -0400

    Remove unnecessary `set` usage.

--- src/schema/directives/RequiresDirective.js
+++ src/schema/directives/RequiresDirective.js
@@ -1,9 +1,8 @@
-import { set } from 'lodash';
 import { SchemaDirectiveVisitor } from 'graphql-tools';
 
 class RequiresDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
-    set(field, 'requiredHttpIncludes', this.args.fields);
+    field.requiredHttpIncludes = this.args.fields;
   }
 }
 
--- src/schema/directives/SensitiveFieldDirective.js
+++ src/schema/directives/SensitiveFieldDirective.js
@@ -1,7 +1,6 @@
 /* Disabling this linting rule since we're conforming to an API. */
 /* eslint-disable class-methods-use-this */
 
-import { set } from 'lodash';
 import { SchemaDirectiveVisitor } from 'graphql-tools';
 
 const HELP_TEXT =
@@ -9,8 +8,8 @@ const HELP_TEXT =
 
 class SensitiveFieldDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
-    set(field, 'isSensitive', true);
-    set(field, 'description', `${field.description} ${HELP_TEXT}`);
+    field.isSensitive = true;
+    field.description = `${field.description} ${HELP_TEXT}`;
   }
 }
 
