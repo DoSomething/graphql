acb63f5a16ab5465fcaea4db1eedcc35e2200491
David Furnes <david@dfurnes.com>
2019-09-17 15:28:15 -0400

    Install Jest & configure test runner.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// For a detailed explanation of each configuration option, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically reset mock instances between each test:
+  clearMocks: true,
+
+  // This is a Node.js application:
+  testEnvironment: 'node',
+};
