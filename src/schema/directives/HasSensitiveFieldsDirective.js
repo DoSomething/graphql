05d258a61f4578a77b99c48738979d3954e6f91a
David Furnes <david@dfurnes.com>
2019-10-18 11:55:41 -0400

    Add support for annotating list return types.

--- src/schema/directives/HasSensitiveFieldsDirective.js
+++ src/schema/directives/HasSensitiveFieldsDirective.js
@@ -1,6 +1,7 @@
 import { values } from 'lodash';
 import { defaultFieldResolver } from 'graphql';
 import { SchemaDirectiveVisitor } from 'graphql-tools';
+import { isListType } from 'graphql/type/definition';
 
 class SensitiveFieldDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
@@ -13,7 +14,11 @@ class SensitiveFieldDirective extends SchemaDirectiveVisitor {
       }
 
       // Get the return type for this field:
-      const type = info.schema.getType(info.returnType.name);
+      const returnType = isListType(info.returnType)
+        ? info.returnType.ofType
+        : info.returnType.name;
+
+      const type = info.schema.getType(returnType);
 
       // If this is the first time we're resolving this type (e.g. User)
       // mark any `@sensitive` fields in the context for later:
