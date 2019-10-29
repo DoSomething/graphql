22b894e4c7ff231930a70d12091f48711fa2ea68
David Furnes <david@dfurnes.com>
2019-10-29 16:50:02 -0400

    Remove unused import.

--- src/repositories/northstar.js
+++ src/repositories/northstar.js
@@ -1,6 +1,6 @@
 import { stringify } from 'qs';
 import logger from 'heroku-logger';
-import { intersection, snakeCase, values } from 'lodash';
+import { intersection, snakeCase } from 'lodash';
 
 import schema from '../schema';
 import config from '../../config';
