97b368d7b4386ce4f4cef19296a7e37e05f4811a
Aaron Schachter <aaronschachter@gmail.com>
2019-11-05 13:27:12 -0800

    Updates to 2019 schema

--- src/repositories/schools.js
+++ src/repositories/schools.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import logger from 'heroku-logger';
 
 import config from '../../config';
 
@@ -7,18 +8,40 @@ mongoose.Promise = global.Promise;
 const DB_URL = config('services.schools.url' || null);
 
 if (DB_URL) {
-  mongoose.connect(DB_URL, { autoIndex: false });
+  mongoose
+    .connect(DB_URL, {
+      autoIndex: false,
+      dbName: config('services.schools.name' || 'greatschools'),
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch(err => logger.error(err.message, err));
 }
 
-const schoolSchema = new mongoose.Schema({
-  _id: String,
-  gsid: String,
-  name: String,
+const schema = new mongoose.Schema({
   city: String,
+  entity: String,
+  name: String,
   state: String,
+  'universal-id': Number,
 });
 
-const School = mongoose.model('School', schoolSchema, 'merged_schools');
+const Directory = mongoose.model('Directory', schema, 'directory');
+
+/**
+ * @param {Object} item
+ * @return {Object}
+ */
+export const transformItem = item => {
+  const { name, city, state } = item;
+
+  return {
+    id: item['universal-id'],
+    name,
+    city,
+    state,
+  };
+};
 
 /**
  * Fetch a school by its ID.
@@ -26,7 +49,13 @@ const School = mongoose.model('School', schoolSchema, 'merged_schools');
  * @param {String} id
  * @return {Object}
  */
-export const getSchoolById = async id => School.findOne({ gsid: id });
+export const getSchoolById = async id => {
+  logger.debug('Finding school', { id });
+
+  const result = await Directory.findOne({ 'universal-id': Number(id) });
+
+  return transformItem(result);
+};
 
 /**
  * Fetch schools by name for given state.
@@ -35,11 +64,17 @@ export const getSchoolById = async id => School.findOne({ gsid: id });
  * @param {String} searchString
  * @return {Object}
  */
-export const searchSchools = async (state, searchString) =>
-  School.find({
+export const searchSchools = async (state, searchString) => {
+  logger.debug('Searching schools', { state, searchString });
+
+  const res = await Directory.find({
+    entity: 'school',
     state,
     name: {
       $regex: searchString,
       $options: 'i',
     },
-  });
+  }).limit(20);
+
+  return res.map(transformItem);
+};
