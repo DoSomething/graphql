8a47767471add7463c9e45d3e1ac974c4accb297
Aaron Schachter <aaronschachter@gmail.com>
2019-11-26 14:14:46 -0800

    Why have you forsaken me

--- src/repositories/rogue.js
+++ src/repositories/rogue.js
@@ -251,9 +251,7 @@ export const getPostById = async (id, context) => {
  */
 export const getPostsByUserId = async (id, page, count, context) => {
   const response = await fetch(
-    `${ROGUE_URL}/api/v3/posts/?filter[northstar_id]=${
-      id
-    }&filter[type]=photo,text&page=${page}&limit=${count}&pagination=cursor`,
+    `${ROGUE_URL}/api/v3/posts/?filter[northstar_id]=${id}&filter[type]=photo,text&page=${page}&limit=${count}&pagination=cursor`,
     authorizedRequest(context),
   );
 
@@ -270,9 +268,7 @@ export const getPostsByUserId = async (id, page, count, context) => {
  */
 export const getPostsByCampaignId = async (id, page, count, context) => {
   const response = await fetch(
-    `${ROGUE_URL}/api/v3/posts/?filter[campaign_id]=${
-      id
-    }&filter[type]=photo,text&page=${page}&limit=${count}&pagination=cursor`,
+    `${ROGUE_URL}/api/v3/posts/?filter[campaign_id]=${id}&filter[type]=photo,text&page=${page}&limit=${count}&pagination=cursor`,
     authorizedRequest(context),
   );
 
@@ -495,9 +491,7 @@ export const getSignupsById = async (ids, options) => {
 
   const idQuery = ids.join(',');
   const response = await fetch(
-    `${ROGUE_URL}/api/v3/signups/?filter[id]=${
-      idQuery
-    }&limit=100&pagination=cursor`,
+    `${ROGUE_URL}/api/v3/signups/?filter[id]=${idQuery}&limit=100&pagination=cursor`,
     options,
   );
   const json = await response.json();
