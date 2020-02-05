7c10505835279fa22f52d67cd2c8821ab55c3add
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-02-05 13:42:14 -0500

    Allow filtering by multiple causes

--- src/schema/rogue.js
+++ src/schema/rogue.js
@@ -346,8 +346,8 @@ const typeDefs = gql`
       page: Int = 1
       "Only return campaigns that are open or closed."
       isOpen: Boolean
-      "Only return campaigns containing this cause."
-      hasCause: String
+      "Only return campaigns containing these causes."
+      causes: [String]
       "How to order the results (e.g. 'id,desc')."
       orderBy: String = "id,desc"
       "The number of results per page."
@@ -361,8 +361,8 @@ const typeDefs = gql`
       after: String
       "Only return campaigns that are open or closed."
       isOpen: Boolean
-      "Only return campaigns containing this cause."
-      hasCause: String
+      "Only return campaigns containing these causes."
+      causes: [String]
       "How to order the results (e.g. 'id,desc')."
       orderBy: String = "id,desc"
     ): CampaignCollection
