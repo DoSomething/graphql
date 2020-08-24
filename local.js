6086f7f47a91128a268deaf87b38ae04ea28e066
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-24 01:10:51 -0400

    Updating local to act as Gateway server.

--- local.js
+++ local.js
@@ -1,44 +1,28 @@
 // Compile ES module syntax on-demand, install 'fetch'
 // polyfill, & load environment variables from '.env'.
 require('dotenv').config();
-fetch = require('node-fetch');
 require = require('esm')(module);
 
 const chalk = require('chalk');
 const boxen = require('boxen');
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
 
-const config = require('./config').default;
-const schema = require('./src/schema').default;
-const dataSources = require('./src/dataSources').default;
+const port = 3000;
 
-const app = express();
+const gateway = new ApolloGateway({
+  serviceList: [
+    // { name: 'activity', url: 'http://localhost:3001' },
+    // { name: 'campaigns', url: 'http://localhost:3002' },
+    { name: 'users', url: 'http://localhost:3003' },
+  ],
+});
+
 const server = new ApolloServer({
-  ...config('graphql'),
-  schema,
-  context: ({ req }) => ({
-    authorization: req.headers.authorization || '',
-  }),
-  dataSources,
+  gateway,
+  subscriptions: false,
 });
 
-app.use((req, res, next) => {
-  // Render customized GraphQL Playground when client asks for HTML:
-  if (req.headers.accept && req.headers.accept.includes('text/html')) {
-    return res.sendFile(`${__dirname}/src/playground.html`);
-  }
-
-  return next();
-});
-
-// Start the Apollo GraphQL server.
-server.applyMiddleware({ app, path: '*' });
-app.listen(config('app.port'), () => {
-  const ENV = chalk.yellow.bold(config('services.displayName'));
-  const message = `${chalk.bold(
-    `Started your local GraphQL server, querying the ${ENV} environment.`,
-  )} \nTry it out by visiting ${config('app.url')}.`;
-
-  console.log(boxen(message, { borderColor: 'yellow', margin: 1, padding: 1 }));
+server.listen({ port }).then(({ url }) => {
+  console.log(`Gateway server ready at ${url}`);
 });
