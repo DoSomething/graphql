3b85fa8ee290d57f634bd130f2860dc496a5cbed
Aaron Schachter <aaronschachter@gmail.com>
2020-09-01 15:36:25 -0700

    Adds example API response in comments

--- src/repositories/airtable.js
+++ src/repositories/airtable.js
@@ -35,6 +35,38 @@ const getAllLocationVotingInformationRecords = async () => {
 
       const json = await response.json();
 
+      /**
+       * Example Airtable API response:
+       *
+       * "records": [
+       *   {
+       *     "id": "recEI2oHq2Hfw38dt",
+       *     "fields": {
+       *         "State": "AL",
+       *         "Voter Registration Deadline": "10/25",
+       *         "Early Voting Starts": "10/5",
+       *         "Absentee Ballot Request Deadline": "10/21",
+       *         "Absentee Ballot Return Deadline Type": "received by",
+       *         "Early Voting Ends": "10/19"
+       *     },
+       *     "createdTime": "2020-08-26T19:12:46.000Z"
+       *   },
+       *   {
+       *     "id": "recHvL0w0KSew1v74",
+       *     "fields": {
+       *         "State": "CA",
+       *         "Voter Registration Deadline": "10/23",
+       *         "Early Voting Starts": "8/30",
+       *         "Absentee Ballot Request Deadline": "10/14",
+       *         "Absentee Ballot Return Deadline Type": "received by",
+       *         "Early Voting Ends": "9/3",
+       *         "Absentee Ballot Return Deadline": "10/18"
+       *     },
+       *     "createdTime": "2020-08-26T19:12:46.000Z"
+       *   },
+       * ...
+       */
+
       const data = json.records.map(record => {
         const { id, fields } = record;
         // Calculate a location field based on State field, then exclude the State field.
