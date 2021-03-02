f3239831df3d91c5bdaf8236a6e9dc823597fdc4
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-03-02 15:16:46 -0500

    Add VR Marketing page secondary cache key (#325)

--- webhook.js
+++ webhook.js
@@ -72,6 +72,7 @@ exports.handler = async event => {
     collectionPage: ['slug'],
     companyPage: ['slug'],
     page: ['slug'],
+    voterRegistrationMarketingPage: ['slug'],
   };
 
   // Clear secondary cache key results from the Contentful cache if applicable.
