0edf29bad2246e67453ea5bf8816f656d73abcfb
Lindsay Maher <lindsaymah3r@gmail.com>
2021-04-27 11:36:58 -0400

    deprecates and makes removes required from caption fields

--- src/schema/contentful/gambit.js
+++ src/schema/contentful/gambit.js
@@ -113,6 +113,10 @@ const typeDefs = gql`
     askPhoto: String!
     "Template that asks user to resend a message with a photo."
     invalidPhoto: String!
+    "Template that asks user to reply with a photo caption."
+    askCaption: String @deprecated(reason: "Field no longer displayed or asked for in user flow.")
+    "Template that asks user to resend a message with a valid photo caption."
+    invalidCaption: String @deprecated(reason: "Field no longer displayed or asked for in user flow.")
     "Template that asks user to reply with why participated."
     askWhyParticipated: String!
     "Template that asks user to resend a message with a valid why participated."
